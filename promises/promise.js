acbb6686533b886533567eb527baad695dd1df4c
obayomi96 <martinsoluwaseun47@gmail.com>
2018-11-22 12:25:55 +0000

    Promises with setTimeout (Using document.write to output directly in browser for testing)

--- promises/promise.js
+++ promises/promise.js
@@ -1,19 +1,20 @@
+
 // Promise
 const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
-// The resolve can be handled anyhow you prefer        
+// The resolve can be handled anyhow you prefer
         resolve("This is the data that gets returned from the server");
     }, 3000);
     
     setTimeout(() => {
-// The reject can also be handled how you prefer        
+// The reject can also be handled how you prefer
         reject("This will through an error if there is one and no data can be gotten from the server")
     }, 5000);
 });
 
 // Your response and error can be handled how you prefer as well
 promise.then(response => {
-    log(response);
+    document.write(response);
 }).catch(error => {
-    log(error);
+    document.write(error);
 })
